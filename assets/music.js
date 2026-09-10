e28d2b1bcb1b5a53038a42e5351cbcbd134cade9
suyashailabs-lang <suyash.ailabs@gmail.com>
2026-09-10 19:59:53 +0530

    Add profession-specific YouTube Music playlists

---
+++ assets/music.js
@@ -0,0 +1,23 @@
+window.PROFESSION_MUSIC = {
+  "Potter": [
+    {title:"Mati Kahe Kumhar Se", url:"https://www.youtube.com/embed/boVAg2hIpNY"},
+    {title:"Maati Ke Matke", url:"https://www.youtube.com/embed/videoseries?list=RDix85Vu2YAI4"},
+    {title:"Panihari", url:"https://www.youtube.com/embed/CablSO-EI4o"},
+    {title:"Ghadlo", url:"https://www.youtube.com/embed/A64yu75kTd4"},
+    {title:"Gorband", url:"https://www.youtube.com/embed/VWHl09F1tNM"}
+  ],
+  "Software Developer": [
+    {title:"Mungaru Maleye", url:"https://www.youtube.com/embed/y7j4pOylYY8"},
+    {title:"Paravashanadenu", url:"https://www.youtube.com/embed/n9oi-wlQD9Y"},
+    {title:"Nenne Tanaka", url:"https://www.youtube.com/embed/JIRg0dM_GUU"},
+    {title:"Tunturu", url:"https://www.youtube.com/embed/i3gEXHI1xXM"},
+    {title:"Naguva Nayana", url:"https://www.youtube.com/embed/Dajcwrykmf8"}
+  ],
+  "Auto Mechanic": [
+    {title:"Zinghat", url:"https://www.youtube.com/embed/luhVm60Wiro"},
+    {title:"Wajle Ki Bara", url:"https://www.youtube.com/embed/hD2g7W_Akfk"},
+    {title:"Kombdi Palali", url:"https://www.youtube.com/embed/unfBlqT0ZPc"},
+    {title:"Zindagi Ek Safar Hai Suhana", url:"https://www.youtube.com/embed/mzxHflxI-es"},
+    {title:"Musafir Hoon Yaaro", url:"https://www.youtube.com/embed/WcsncpEWQZI"}
+  ]
+};
